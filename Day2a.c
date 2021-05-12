27d106979f387177c7050123a03d1750ca54f05e
AmalTJohnson <77672991+AmalTJohnson@users.noreply.github.com>
2021-05-12 20:11:59 +0530

    Add files via upload

---
+++ Day2a.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+   int num1;
+   
+   
+   scanf("%d",&num1);
+   
+   
+       
+       sqrt(num1);
+       printf("%d");
+       
+     
+       
+       
+       
+       return 0;
+   } 
---
+++ Day2b.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	int num;
+   int num1;
+   
+   
+   scanf("%d",&num); 
+   num1 =  sqrt(num);
+   printf("%d",num1);
+       
+     
+       
+       
+       
+       return 0;
+   } 

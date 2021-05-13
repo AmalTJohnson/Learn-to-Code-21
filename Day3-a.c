b6de5501a693b5dd1ad4d0519e50f6db790c6d74
AmalTJohnson <77672991+AmalTJohnson@users.noreply.github.com>
2021-05-13 15:45:19 +0530

    Create Day3-a.c

---
+++ Day3-a.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+   float side;
+   float rad;
+   float base,hite ;
+    
+    scanf("%f",&side);
+    scanf("%f",&rad);
+    scanf("%f",&base);
+    scanf("%f",&hite);
+    
+    printf("%.0f\n", side*side );
+    printf("%.1f\n", 3.14*rad*rad);
+    printf("%.2f\n", .5*base*hite);
+    
+    return 0;
+}
+    
+    

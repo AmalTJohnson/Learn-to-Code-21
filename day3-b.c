d294132d52528dafa0e87d0a81076d98c11b54ba
AmalTJohnson <77672991+AmalTJohnson@users.noreply.github.com>
2021-05-13 15:42:57 +0530

    Create day3-b.c

---
+++ day3-b.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float F,F2;
+
+    scanf("%f",&F);
+    F2=(F-32)*5/9;
+    printf("%.2f\n",F2);
+    
+        
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}

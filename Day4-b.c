2b6a6815c5e36b189831dfd2d402ddff67130f09
AmalTJohnson <77672991+AmalTJohnson@users.noreply.github.com>
2021-05-15 10:25:44 +0530

    Create Day4-b.c

---
+++ Day4-b.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num3, max, min;
+
+ 
+    scanf("%d%d%d", &num1, &num2, &num3);
+    if(num1 > num2)
+    { if(num1 > num3)
+        {
+          max = num1;
+        }
+        else
+        {
+          max = num3;
+        }
+    }
+    else
+    {
+      if(num2 > num3)
+    {
+         max = num2;
+        }
+        else
+        {
+         max = num3;
+        }
+    }
+        printf("%d\n", max);
+        
+        
+         if(num1 < num2)
+    { if(num1 < num3)
+        {
+          min = num1;
+        }
+        else
+        {
+          min = num3;
+        }
+    }
+    else
+    {
+      if(num2 < num3)
+    {
+         min = num2;
+    }
+        else
+        {
+         min = num3;
+        }
+    }
+    printf("%d\n", min);
+        
+
+    return 0;
+}

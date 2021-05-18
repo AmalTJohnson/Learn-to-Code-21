1419bd696c6c015be813004e17e131e4534d2bab
AmalTJohnson <77672991+AmalTJohnson@users.noreply.github.com>
2021-05-18 13:26:19 +0530

    Create Day8-b.c

---
+++ Day8-b.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int v;
+    float a,b;
+    
+    scanf("%d",&v);
+    scanf("%f%f",&a,&b);
+    printf("1.Addition\n");
+    printf("2.Subtraction\n");
+    printf("3.Multiplication\n");
+    printf("4.Division\n");
+    
+    switch(v)
+    {
+        case 1: printf("%.0f",a+b);
+            break;
+            case 2: printf("%.0f\n",a-b);
+            break;
+            case 3: printf("%.0f\n",a*b);
+            break;
+            case 4: if (b==0)
+                {printf("Cannot be determined");}
+            else printf("%.1f\n",a/b);
+                
+            break;
+      
+    }
+
+  
+    return 0;
+}

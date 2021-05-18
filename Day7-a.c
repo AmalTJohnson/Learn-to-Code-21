1fb0b5f427e2fead8db5c415eebcc15be7b78723
AmalTJohnson <77672991+AmalTJohnson@users.noreply.github.com>
2021-05-18 15:29:03 +0530

    Update Day7-a.c

--- Day7-a.c
+++ Day7-a.c
@@ -1,3 +1,5 @@
+/* Q - WACP to accept marks of three subjects.. Calculate the percentage and respective grade according to following: Percentage >= 90% : Grade A Percentage >= 80% : Grade B Percentage >= 70% : Grade C Percentage >= 60% :
+       Grade D Percentage >= 40% : Grade E Percentage < 40% : Grade F */
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
